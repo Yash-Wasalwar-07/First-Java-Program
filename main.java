495ee02a8c63908d723640d420e5d700114551de
Yash-Wasalwar-07 <71366827+Yash-Wasalwar-07@users.noreply.github.com>
2021-01-10 23:57:15 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {     //class name follows Pascal convention
+
+    public static void main(String[] args) {      //function name follows camelcase convention
+        //write your code here
+        System.out.println("Hello World");
+    }
+}
